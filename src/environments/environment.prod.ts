b2f6f6d40505c2e3b8a4faf1174bce5e2529df54
Benard Mathu <mathubenard@gmail.com>
2023-03-23 14:51:02 +0300

    Update: change environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   firebase: {
     projectId: 'ben-mathu-portfolio',
-    appId: '1:512645300438:web:ec20863554ed936cb28977',
+    appId: '1:512645300438:web:e1b41a9724275508b28977',
     databaseURL: 'https://ben-mathu-portfolio-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ben-mathu-portfolio.appspot.com',
     locationId: 'europe-west',
     apiKey: 'AIzaSyASvEpLdxPFboli1hzO8AFrth1cpc9Ate0',
     authDomain: 'ben-mathu-portfolio.firebaseapp.com',
     messagingSenderId: '512645300438',
-    measurementId: 'G-059WZFGF3R',
+    measurementId: 'G-3S6VR7XQRY',
   },
   production: true
 };
